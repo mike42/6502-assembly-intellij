e04938442c6f027fce6dbfc4b03f9846b902cac5
Michael Billington <michael.billington@gmail.com>
2022-01-01 22:08:34 +1100

    update version range in plugin.xml, exclude CLion, add runPluginVerifier config to help in future

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -exu -o pipefail
 rm -Rf build src/main/gen src/main/resources/projectTemplates/*.zip
 (cd templates && zip --recurse-paths "../src/main/resources/projectTemplates/Generic 6502 Project.zip" "Generic 6502 Project/")
 ./gradlew generateAsmLexer generateAsmParser verifyPlugin buildPlugin
+./gradlew runPluginVerifier
