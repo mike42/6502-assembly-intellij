fca42104f69855a4c76bb79e7e8f4c02ced1241f
Michael Billington <michael.billington@gmail.com>
2026-06-21 00:43:38 +1000

    remove project template - this functionalty has changed and did not work correctly on current IDE versions

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -exu -o pipefail
 rm -Rf build src/main/gen src/main/resources/projectTemplates/*.zip
-(cd "templates/Generic 6502 Project" && zip --recurse-paths "../../src/main/resources/projectTemplates/Generic 6502 Project.zip" root0/ .idea/)
 # buildPlugin runs the structure/configuration checks; verifyPlugin (2.x plugin)
 # replaces the old runPluginVerifier task and runs the IntelliJ Plugin Verifier.
 ./gradlew generateAsmLexer generateAsmParser buildPlugin
